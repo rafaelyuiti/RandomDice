8c3422dd4792c23c700ac7da38c3e17677ff74e6
rafaelyuiti <rafael_y91@hotmail.com>
2020-03-15 12:04:37 -0300

    primeiro comit xcode

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //poderia ser var, mas é let pq o array é estático
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        //who . what = value
+        diceImageView1.image = diceArray[Int.random(in:0...5)]
+        diceImageView2.image = diceArray.randomElement() //vê quantos elementos ja possui e randomiza
+        
+        print("teste de commit")
+        
     }
-
+    
 
 }
 
